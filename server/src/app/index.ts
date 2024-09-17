2b1a018a76413b549b68f7a40f7c967ed9bef196
Abhi <abhinav.panwarr@gmail.com>
2024-09-17 20:54:32 +0530

    graphql server updates

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,22 +1,24 @@
 import express from 'express'
 import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
+import bodyParser from 'body-parser';
 
 export async function initServer() {
     const app = express();
+
+    app.use(bodyParser.json())
     const graphqlServer = new ApolloServer({
         typeDefs: `
             type Query {
-            
+                sayHello: String
             }
 
-            type Mutation {
-                
-            }
         `,
         resolvers: {
-            Query: {},
-            Mutation: {}
+            Query: {
+                sayHello: () => `Hello from graphql Server`
+            },
+           
         },
     });
 
