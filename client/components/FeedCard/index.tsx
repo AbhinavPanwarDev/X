22ede7b02aef33335e5dfec8848142064d6bacc8
Abhi <abhinav.panwarr@gmail.com>
2024-09-17 12:52:41 +0530

    final leftSidebar

---
+++ client/components/FeedCard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const FeedCard: React.FC = () => {
+    return <div>
+        <div className='grid grid-cols-12 '></div>
+    </div>
+};
+
+export default FeedCard
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -10,6 +10,7 @@ import { GiCharacter } from "react-icons/gi";
 import { CgMoreO } from "react-icons/cg";
 
 
+import FeedCard from '@/components/FeedCard';
 import React from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
@@ -54,20 +55,22 @@ export default function Home() {
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen">
-        <div className="col-span-3 p-5 ">
-          <div className='text-3xl w-fit px-6 py-5 hover:bg-gray-700 rounded-full cursor-pointer transition-all'>
+        <div className="col-span-3 px-9 py-3">
+          <div className='text-3xl w-fit px-6 py-2 ml-10 hover:bg-gray-700 rounded-full cursor-pointer transition-all'>
             <FaXTwitter />
           </div>
-          <div  className='mt-2 text-xl font-bold'>
+          <div  className='mt-2 text-xl font-bold px-10 '>
             <ul>
             {sidebarMenuItems.map(item => <li className='flex justify-start items-center gap-3 mt-2 hover:bg-gray-700 rounded-full px-7 py-3 w-fit cursor-pointer transition-all' key={item.title}><span>{item.icon}</span><span>{item.title}</span></li>)}
             </ul>
-            <div className='mt-5 px-12'>
+            <div className='mt-5 px-6'>
               <button className='bg-[#1d9bf0] rounded-full p-3 w-full'>Tweet</button>
             </div>
           </div>
         </div>
-        <div className="col-span-5 border-r-[1px] border-l-[1px] border-slate-700"></div>
+        <div className="col-span-5 border-r-[1px] border-l-[1px] border-slate-700">
+          <FeedCard />
+        </div>
         <div className="col-span-4"></div>
       </div>
     </div>
