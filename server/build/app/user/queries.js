0fbe8c95266072f1591a1da370823756d6cac7fb
Abhi <abhinav.panwarr@gmail.com>
2024-09-21 23:48:56 +0530

    jwt resolverUpdate

--- server/build/app/user/queries.js
+++ server/build/app/user/queries.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.queries = void 0;
 exports.queries = `
-    verifyGoogleToken(token: String!): String
+    verifyGoogleToken (token: String!): String
 `;
