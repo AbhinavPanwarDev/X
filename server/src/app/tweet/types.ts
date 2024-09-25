142f9d42798ccdc4368a148a82c42a073a30776a
Abhi <abhinav.panwarr@gmail.com>
2024-09-25 23:40:18 +0530

    Tweet Schema

---
+++ server/src/app/tweet/types.ts
@@ -0,0 +1,9 @@
+export const types = `
+    type Tweet{
+        id: ID!
+        content: String!
+        imageURL: String
+
+        author: User
+    }
+`;
--- server/src/app/user/types.ts
+++ server/src/app/user/types.ts
@@ -1,12 +1,13 @@
 export const types = `#graphql
 
-
     type user {
         id: ID!
         firstName: String!
         lastName: String
         email: String!
         profileImageURL: String
+
+        tweets: [Tweet]
     }
 
 `;
